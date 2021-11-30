6a814d2a3ba1cd41248ed7139fa2e26855a0ca75
meli-diteixeira <dihogo.teixeira@mercadolivre.com>
2021-11-30 19:09:36 -0300

    adicionando comentario ao bucket

--- aula-06/state/bucket.tf
+++ aula-06/state/bucket.tf
@@ -1,4 +1,5 @@
 resource "google_storage_bucket" "state" {
+  # Utilizar um nome diferente para o bucket pois ele é unico
   name          = "4linuxstateterraform"
   location      = "US"
   force_destroy = true
