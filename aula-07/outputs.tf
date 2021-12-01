c110f5590baac7e2dd94c42b21fc07c2a57a9377
meli-diteixeira <dihogo.teixeira@mercadolivre.com>
2021-12-01 18:12:48 -0300

    criando exemplo aula-7

--- aula-07/outputs.tf
+++ aula-07/outputs.tf
@@ -1,19 +1,19 @@
 output "name" {
-  value = module.instances.name
+  value = module.instance.name
 }
 
 output "instance_id" {
-  value = module.instances.instance_id
+  value = module.instance.instance_id
 }
 
 output "cpu_platform" {
-  value = module.instances.cpu_platform
+  value = module.instance.cpu_platform
 }
 
 output "external_ip" {
-    value = module.instances.external_ip
+    value = module.instance.external_ip
 }
 
 output "internal_ip" {
-    value = module.instances.internal_ip
+    value = module.instance.internal_ip
 }
