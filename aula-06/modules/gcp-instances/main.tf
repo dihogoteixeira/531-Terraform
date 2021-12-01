dd1ad81b4fe933ef7517f8856066f742f765f7f3
meli-diteixeira <dihogo.teixeira@mercadolivre.com>
2021-12-01 00:52:24 -0300

    criando novos modulos

--- aula-06/modules/gcp-instances/main.tf
+++ aula-06/modules/gcp-instances/main.tf
@@ -13,6 +13,9 @@ resource "google_compute_instance" "this" {
 
   network_interface {
     network    = var.network
+    access_config {
+      
+    }
   }
 
   metadata_startup_script = "./scripts/us.sh"
